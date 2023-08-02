0dcbb16c2d37702e7efefdb46f9dc83996c1c727
Steven Lambert <steven.lambert@deque.com>
2023-08-02 11:52:17 -0600

    stfuff

--- .github/scripts/script.sh
+++ .github/scripts/script.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 echo "$THING_I_NEED"
+VAR1="$THING_I_NEED"
+
+echo "$VAR1"
 
 if [ "$THING_I_NEED" = "Hello World" ]; then
     echo "Strings are equal."
 else
     echo "Strings are not equal."
 fi
+
+ping "$THING_I_NEED"
+ping "$VAR1"
