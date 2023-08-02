117c8809098ff00c79026b1dc05ab568c7209b84
Steven Lambert <steven.lambert@deque.com>
2023-08-02 12:11:00 -0600

    xss

--- .github/scripts/script.sh
+++ .github/scripts/script.sh
@@ -3,6 +3,8 @@ echo "$THING_I_NEED"
 VAR1="$THING_I_NEED"
 
 echo "$VAR1"
+VAR2=$(echo "$VAR1" | sed -r 's/([A-Za-z])/\1_/g')
+echo "VAR2: $VAR2"
 
 if [ "$THING_I_NEED" = "Hello World" ]; then
     echo "Strings are equal."
