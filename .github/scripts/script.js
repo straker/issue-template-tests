a91c33196552c236a5b449fe0446df7f0903e565
Steven Lambert <steven.lambert@deque.com>
2023-08-31 09:24:45 -0600

    moar

--- .github/scripts/script.js
+++ .github/scripts/script.js
@@ -2,8 +2,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const bodyLines = github.context.payload.pull_request.body.split('\n');
-const footer = bodyLines[bodyLines.length - 1];
+const bodyLines = github.context.payload.pull_request.body.split(/[\r\n]+/);
+const footer = bodyLines[bodyLines.length - 1].toLowerCase();
 
 console.log('bodyLines:', bodyLines);
 console.log('footer:', footer);
+
+if (
+  !(
+    footer.startsWith('closes: #') ||
+    footer.startsWith('ref: ') ||
+    footer.startsWith('refs: ') ||
+    footer.startsWith('qa notes: ') ||
+    footer.startsWith('no qa required')
+  )
+) {
+  console.error('PR Footer WRONG!');
+  return;
+}
+
+console.log('PR footer has correct syntax');
